3242a671a8f1838ee34c17483b48f4f1128161d7
Nguyễn Mạnh Hiệp <nguyen.fs90@gmail.com>
2020-10-28 07:25:45 +0900

    esm module

---
+++ 01-the-module-system/09-esm-systax/main3.js
@@ -0,0 +1,5 @@
+import { log, Logger } from './logger'
+
+log('hello world')
+const logger = new Logger('DEFAULT')
+logger.log('Hello world')
---
+++ 01-the-module-system/09-esm-systax/main5.js
@@ -0,0 +1,4 @@
+import { log as log2 } from './logger'
+const log = console.log
+log('message from log')
+log2('message from log2')
